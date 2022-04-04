ea261d7eb44b6d894d172a5fc7473b2444177314
Pitus660 <pitus660@gmail.com>
2022-04-04 15:56:43 +0200

    ADD custom config redis

--- lib/gush/configuration.rb
+++ lib/gush/configuration.rb
@@ -1,42 +1,43 @@
 module Gush
-  class Configuration
-    attr_accessor :concurrency, :namespace, :redis_url, :ttl, :locking_duration, :polling_interval
+	class Configuration
+		attr_accessor :concurrency, :namespace, :redis_url, :ttl, :locking_duration, :polling_interval, :redis_config
 
-    def self.from_json(json)
-      new(Gush::JSON.decode(json, symbolize_keys: true))
-    end
+		def self.from_json(json)
+			new(Gush::JSON.decode(json, symbolize_keys: true))
+		end
 
-    def initialize(hash = {})
-      self.concurrency      = hash.fetch(:concurrency, 5)
-      self.namespace        = hash.fetch(:namespace, 'gush')
-      self.redis_url        = hash.fetch(:redis_url, 'redis://localhost:6379')
-      self.gushfile         = hash.fetch(:gushfile, 'Gushfile')
-      self.ttl              = hash.fetch(:ttl, -1)
-      self.locking_duration = hash.fetch(:locking_duration, 2) # how long you want to wait for the lock to be released, in seconds
-      self.polling_interval = hash.fetch(:polling_internal, 0.3) # how long the polling interval should be, in seconds
-    end
+		def initialize(hash = {})
+			self.concurrency = hash.fetch(:concurrency, 5)
+			self.namespace = hash.fetch(:namespace, 'gush')
+			self.redis_url = hash.fetch(:redis_url, 'redis://localhost:6379')
+			self.redis_config = hash.fetch(:redis_config, {})
+			self.gushfile = hash.fetch(:gushfile, 'Gushfile')
+			self.ttl = hash.fetch(:ttl, -1)
+			self.locking_duration = hash.fetch(:locking_duration, 2) # how long you want to wait for the lock to be released, in seconds
+			self.polling_interval = hash.fetch(:polling_internal, 0.3) # how long the polling interval should be, in seconds
+		end
 
-    def gushfile=(path)
-      @gushfile = Pathname(path)
-    end
+		def gushfile=(path)
+			@gushfile = Pathname(path)
+		end
 
-    def gushfile
-      @gushfile.realpath if @gushfile.exist?
-    end
+		def gushfile
+			@gushfile.realpath if @gushfile.exist?
+		end
 
-    def to_hash
-      {
-        concurrency:      concurrency,
-        namespace:        namespace,
-        redis_url:        redis_url,
-        ttl:              ttl,
-        locking_duration: locking_duration,
-        polling_interval: polling_interval
-      }
-    end
+		def to_hash
+			{
+				concurrency: concurrency,
+				namespace: namespace,
+				redis_url: redis_url,
+				ttl: ttl,
+				locking_duration: locking_duration,
+				polling_interval: polling_interval
+			}
+		end
 
-    def to_json
-      Gush::JSON.encode(to_hash)
-    end
-  end
+		def to_json
+			Gush::JSON.encode(to_hash)
+		end
+	end
 end
